1b9769f1b122ba952546137187c3a09bd73879b7
Omar <omaralamiaroussi@gmail.com>
2022-02-01 16:11:57 +0100

    Remove watchlist

--- app/src/main/java/com/example/movieuitemplate/adapters/WatchlistsActivityAdapter.java
+++ app/src/main/java/com/example/movieuitemplate/adapters/WatchlistsActivityAdapter.java
@@ -6,12 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.movieuitemplate.R;
+import com.example.movieuitemplate.models.Movie;
 import com.example.movieuitemplate.models.WatchListItem;
+import com.example.movieuitemplate.models.Watchlist;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
@@ -53,6 +60,7 @@ public class WatchlistsActivityAdapter extends BaseAdapter {
             holder.watchlistImg = (ImageView) convertView.findViewById(R.id.watchlistImg);
             holder.watchlistName = (TextView) convertView.findViewById(R.id.watchlistTitle);
             holder.watchlistCount = (TextView) convertView.findViewById(R.id.watchlistCount);
+           // holder.removeBtn = (ImageButton) convertView.findViewById(R.id.removeBtn);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -63,6 +71,22 @@ public class WatchlistsActivityAdapter extends BaseAdapter {
        // Glide.with(context).load("https://image.tmdb.org/t/p/original" +listData.get(position).getImage()).into(holder.watchlistImg);
         holder.watchlistName.setText(watchlist.getName());
         holder.watchlistCount.setText("Movies: " + watchlist.getCount());
+      /*  holder.removeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(context,watchlistId, Toast.LENGTH_SHORT).show();
+                FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+                //Log.e("ref", reference.toString());
+                reference.child("Watchlists").child(String.valueOf(watchlist.getId())).removeValue();
+
+
+                WatchListItem watchlisttoremove = listData.get(position);
+                listData.remove(watchlisttoremove);
+                notifyDataSetChanged();
+            }
+        });*/
+
 
         //int imageId = this.getMipmapResIdByName(wa.getFlagName());
         //holder.flagView.setImageResource(imageId);
@@ -74,6 +98,7 @@ public class WatchlistsActivityAdapter extends BaseAdapter {
         ImageView watchlistImg;
         TextView watchlistName;
         TextView watchlistCount;
+       // ImageButton removeBtn;
     }
 
 }
